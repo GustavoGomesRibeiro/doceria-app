6edbabef65cb77e8e2dd63493e15ef265fc91c9c
gustavogomesribeiro <gustavo_roox@hotmail.com>
2024-11-26 18:44:06 -0300

    fix: adjustments placeholder

--- src/components/card/page.tsx
+++ src/components/card/page.tsx
@@ -16,44 +16,46 @@ const Card: FC<CardProps> = ({ produtos }) => {
   const addProduct = useCartStore((state) => state.addProduct);
 
   return (
-    <div className="md:grid md:grid-cols-4 md:gap-4 p-5">
-      <ConditionalRender condition={Boolean(produtos.length)}>
-        {produtos.map((produto) => (
-          <div
-            key={produto.id}
-            className="shadow-md shadow-gray-500 mb-5 border-solid border-4 border-black rounded-b-md"
-          >
-            <div className="bg-gray-500 rounded-tl-lg rounded-tr-lg">
-              <Image
-                src="/400.png"
-                placeholder="empty"
-                width={500}
-                height={500}
-                alt="Picture of the author"
-              />
-            </div>
-            <div className="flex bg-white p-5 rounded-lg h-40 flex-col justify-between">
-              <Text className="font-bold my-2">{produto.titulo}</Text>
-              <Text className="text-base">{produto.descricao}</Text>
+    <>
+      <div className="md:grid md:grid-cols-4 md:gap-4 p-5">
+        <ConditionalRender condition={Boolean(produtos.length)}>
+          {produtos.map((produto) => (
+            <div
+              key={produto.id}
+              className="shadow-md shadow-gray-500 mb-5 border-solid border-4 border-black rounded-b-md"
+            >
+              <div className="bg-gray-500 rounded-tl-lg rounded-tr-lg">
+                <Image
+                  src="/400.png"
+                  placeholder="empty"
+                  width={500}
+                  height={500}
+                  alt="Picture of the author"
+                />
+              </div>
+              <div className="flex bg-white p-5 rounded-lg h-40 flex-col justify-between">
+                <Text className="font-bold my-2">{produto.titulo}</Text>
+                <Text className="text-base">{produto.descricao}</Text>
 
-              <div className="flex justify-between items-center">
-                <Text className="text-[#DB2777] font-bold">
-                  R$ {produto.preco}
-                </Text>
-                <Button
-                  className="bg-[#DB2777] rounded-full p-2 shadow-md shadow-gray-500 hover:transform hover:-translate-y-1.5 transition-transform duration-250 ease-[cubic-bezier(0.3,0.7,0.4,1.5)]"
-                  addProduct={() => addProduct(produto)}
-                >
-                  <Text className="text-white">Adicionar</Text>
-                </Button>
+                <div className="flex justify-between items-center">
+                  <Text className="text-[#DB2777] font-bold">
+                    R$ {produto.preco}
+                  </Text>
+                  <Button
+                    className="bg-[#DB2777] rounded-full p-2 shadow-md shadow-gray-500 hover:transform hover:-translate-y-1.5 transition-transform duration-250 ease-[cubic-bezier(0.3,0.7,0.4,1.5)]"
+                    addProduct={() => addProduct(produto)}
+                  >
+                    <Text className="text-white">Adicionar</Text>
+                  </Button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </ConditionalRender>
+          ))}
+        </ConditionalRender>
+      </div>
 
       <ConditionalRender condition={produtos.length <= 0}>
-        <div className="flex justify-start items-start">
+        <div className="md:grid md:grid-cols-4 md:gap-4 p-5">
           <div className="p-5">
             <Placeholder />
           </div>
@@ -65,7 +67,7 @@ const Card: FC<CardProps> = ({ produtos }) => {
           </div>
         </div>
       </ConditionalRender>
-    </div>
+    </>
   );
 };
 
